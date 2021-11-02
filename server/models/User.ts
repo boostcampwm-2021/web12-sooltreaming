3869d07f0f236f9ea2b5b03d3132881fe7589304
alittlekitten <alittlekitten@github.com>
2021-11-02 16:20:42 +0900

    :card_file_box: DB 스키마 변경
    RDB형태의 구조를 NoSQL형태로 변경

--- server/models/User.ts
+++ server/models/User.ts
@@ -5,10 +5,26 @@ interface User {
   naverId: string;
   nickname: string;
   imgUrl: string;
+
+  chatCount: number;
+  hookCount: number;
+  pollCount: number;
+  closeupCount: number;
+  dieCount: number;
+  speakCount: number;
+  starterCount: number;
+  totalSeconds: number;
+
+  title: Array<String>;
+
+  sendFriend: Array<String>;
+  receiveFriend: Array<String>;
+  friend: Array<String>;
 }
 
 const userSchema = new Schema<User>(
   {
+    // User
     githubId: String,
     naverId: String,
     nickname: {
@@ -18,6 +34,60 @@ const userSchema = new Schema<User>(
     imgUrl: {
       type: String,
       required: true,
+    },
+
+    // Statistic
+    chatCount: {
+      type: Number,
+      default: 0,
+    },
+    hookCount: {
+      type: Number,
+      default: 0,
+    },
+    pollCount: {
+      type: Number,
+      default: 0,
+    },
+    closeupCount: {
+      type: Number,
+      default: 0,
+    },
+    dieCount: {
+      type: Number,
+      default: 0,
+    },
+    speakCount: {
+      type: Number,
+      default: 0,
+    },
+    starterCount: {
+      type: Number,
+      default: 0,
+    },
+    totalSeconds: {
+      type: Number,
+      default: 0,
+    },
+
+    // Title
+    title: {
+      type: [String],
+      default: [],
+    },
+
+    // Friend
+    sendFriend: {
+      type: [String],
+      default: [],
+    },
+    receiveFriend: {
+      type: [String],
+      default: [],
+    },
+    friend: {
+      type: [String],
+      default: [],
     },
   },
   { timestamps: true },
