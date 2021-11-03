d67551fad415e3cdd63295d3a970c59b20807fb8
pyo-sh <pygosky@gmail.com>
2021-11-03 19:20:09 +0900

    :lipstick: 전역 스타일 설정

--- client/src/GlobalStyle.js
+++ client/src/GlobalStyle.js
@@ -3,7 +3,23 @@ import { normalize } from 'styled-normalize';
 
 const GlobalStyle = createGlobalStyle`
   ${normalize}
-
+  // Noto Sans Korean (한국어만)
+  @font-face {
+    src: url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+    font-family: 'Noto Sans KR';
+    unicode-range: U+AC00-U+D7A3;
+  }
+  body {
+    width: 100vw;
+    height: 100vh;
+    padding: 0;
+    margin: 0;
+    font-family: 'Merriweather', 'Noto Sans KR', sans-serif;
+  }
+  #root {
+    width: 100%;
+    height: 100%;
+  }
   * {
     box-sizing: border-box;
   }
