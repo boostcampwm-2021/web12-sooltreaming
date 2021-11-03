228fbf73ac8db5d9a81121c63562c6a60ad3f743
alittlekitten <dlscjs8646@gmail.com>
2021-11-04 01:04:12 +0900

    :recycle: pipe 함수 복구 및 사용, signaling 분리
    server/controller/socket/signaling.ts 추가, server/loader/socket.ts에서 pipe 함수 사용

---
+++ server/controller/socket/signaling.ts
@@ -0,0 +1,28 @@
+const Signaling = (socket) => {
+  socket.on('joinRoom', (roomCode) => {
+    console.log('룸참여');
+    socket.join(roomCode);
+    socket.to(roomCode).emit('welcome');
+  });
+  
+  socket.on('offer', (offer, roomCode) => {
+    console.log('offer전송');
+    socket.to(roomCode).emit('offer', offer);
+  });
+  
+  socket.on('answer', (answer, roomCode) => {
+    console.log('answer전송');
+    socket.to(roomCode).emit('answer', answer);
+  });
+  
+  socket.on("ice", (ice, roomCode) => {
+    console.log('ice전송');
+    socket.to(roomCode).emit("ice", ice);
+  })
+  return socket;
+}
+
+export default Signaling
+
+
+
--- server/loader/socket.ts
+++ server/loader/socket.ts
@@ -1,11 +1,8 @@
 import { Socket, Server } from 'socket.io';
+import signaling from '../controller/socket/signaling';
+import pipe from 'utils/pipe';
 
-interface Chat {
-  message: string;
-  room_id: string;
-}
-
-function socketLoader(server, app): any {
+const socketLoader = (server, app): any => {
   const io = new Server(server, {
     cors: {
       origin: `http://${process.env.FRONT_HOST}:${process.env.FRONT_PORT}`,
@@ -16,14 +13,13 @@ function socketLoader(server, app): any {
   io.on('connection', (socket: Socket) => {
     console.log('socket connection!!', socket.id);
 
-    socket.on('send_message', (chat: Chat) => {
-      socket.broadcast.to(chat.room_id).emit('receive_message', chat);
-    });
+    pipe(signaling)(socket);
 
     socket.on('disconnect', () => {
       console.log('disconnect socket!!' + socket.id);
     });
   });
+
   return app;
 }
 
