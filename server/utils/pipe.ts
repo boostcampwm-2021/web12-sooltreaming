3dc9786c88c63817fe9a542e0ebe3bd6d63dee07
alittlekitten <dlscjs8646@gmail.com>
2021-11-03 14:50:56 +0900

    :sparkles: Loader 간소화, socket.io 연결, cors 설정

--- server/utils/pipe.ts
+++
@@ -1,6 +0,0 @@
-function pipe(...functions: any): any {
-  return (first_value: any) =>
-    functions.reduce((prev_value: any, func: any) => func(prev_value), first_value);
-}
-
-export default pipe;
