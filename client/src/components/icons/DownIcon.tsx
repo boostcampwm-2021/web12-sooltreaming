ef471f9ab68bc3a2289e4d640dc6d6215585ceea
pyo-sh <pygosky@gmail.com>
2021-11-09 18:51:50 +0900

    :lipstick: Icon 추가 및 stroke-width를 strokeWidth로

--- client/src/components/icons/DownIcon.tsx
+++ client/src/components/icons/DownIcon.tsx
@@ -3,22 +3,21 @@ import type { iconPropsType } from '@components/icons';
 
 const DownIcon = ({ width, height, fill, stroke }: iconPropsType): React.ReactElement => {
   return (
-    <svg  
+    <svg
       width={width ?? 18}
       height={height ?? 10}
       viewBox="0 0 18 10"
       fill={fill ?? 'none'}
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path 
-        d="M17 0.999999L9 9L1 1" 
-        stroke={ stroke ?? "white"} 
-        stroke-width="2" 
-        stroke-linecap="round" 
-        stroke-linejoin="round"
+      <path
+        d="M17 0.999999L9 9L1 1"
+        stroke={stroke ?? 'white'}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
-
   );
 };
 
