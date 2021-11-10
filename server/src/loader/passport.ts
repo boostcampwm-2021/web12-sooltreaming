c6887249ce1bf6d6a12708ffa38db92eb69dd316
pyo-sh <pygosky@gmail.com>
2021-11-10 18:18:35 +0900

    :recycle: passport 에서 session 저장할 때 유저 id만 저장하기

--- server/src/loader/passport.ts
+++ server/src/loader/passport.ts
@@ -1,15 +1,18 @@
 import passport from 'passport';
 import gitHubStrategy from '@controller/passport/github';
+import User from '@models/User';
+import type { ObjectId } from 'mongoose';
 
 const passportLoader = (app) => {
-  // Session 설정
-  const passToRequest = (user, done) => {
-    done(null, user);
-  };
   // serializeUser : Session에 유저 정보를 저장
-  passport.serializeUser(passToRequest);
+  passport.serializeUser((user: { _id: ObjectId }, done) => {
+    done(null, user._id);
+  });
   // deserializeUser : 페이지 접근 때마다 Session에서 Request 객체에 넘겨줄 값 설정
-  passport.deserializeUser(passToRequest);
+  passport.deserializeUser(async (id, done) => {
+    const user = User.findById(id);
+    done(null, user);
+  });
 
   // 전략 설정
   passport.use(gitHubStrategy);
