f15347e91743c410063e9b518d67818962bbffc0
Ga-Yeon Park <rkdusdd52@naver.com>
2021-11-10 21:21:40 +0900

    :recycle: 메뉴 컴포넌트 리팩토링

--- client/src/components/chat-room/Menu.tsx
+++ client/src/components/chat-room/Menu.tsx
@@ -1,32 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import type { MenuPropTypes } from '@components/chat-room/ChatRoom';
 
 import { Wrapper, TopBar, CloseButton } from '@components/chat-room/ChatMenu.style';
 import Chat from '@components/chat-room/Chat';
 
-const RouteMenu = ({ menuType }) => {
-  switch (menuType) {
-    case '방장':
-      return <div></div>;
-    case '게임':
-      return <div></div>;
-    case '참가자':
-      return <div></div>;
-    case '채팅':
-      return <Chat />;
-    case '설정':
-      return <div></div>;
-    case '클로즈업':
-      return <div></div>;
-    case '건배':
-      return <div></div>;
-
-    default:
-      return <div></div>;
-  }
-};
-
 const Menu: React.FC<MenuPropTypes> = ({ menuType, setMenuType }) => {
+  const MENU = useMemo(() => {
+    return {
+      방장: <></>,
+      게임: <></>,
+      참가자: <></>,
+      설정: <></>,
+      채팅: <Chat />,
+      클로즈업: <></>,
+      건배: <></>,
+    };
+  }, []);
+
+  const RouteMenu = ({ menuType }) => {
+    return MENU[menuType] || <></>;
+  };
+
   if (!menuType) return <></>;
   return (
     <Wrapper>
