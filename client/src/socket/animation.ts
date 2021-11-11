644be789c2b41b09a7c27d1b52d559848c803c3b
alittlekitten <dlscjs8646@gmail.com>
2021-11-11 17:35:59 +0900

    :bug: 클라이언트에서 방별 애니메이션 처리 구현

--- client/src/socket/animation.ts
+++ client/src/socket/animation.ts
@@ -5,6 +5,7 @@ const CHEERS = 'CHEERS';
 const animation = (socket: Socket) => (closure: any) => {
   const { setIsCheers } = closure;
   console.log(setIsCheers);
+  console.log(socket, '접속소켓');
 
   socket.on(CHEERS, () => {
     console.log('이벤트받았다~');
@@ -14,7 +15,9 @@ const animation = (socket: Socket) => (closure: any) => {
     }, 5000);
   });
 
-  const activateCheers = () => socket.emit(CHEERS);
+  const activateCheers = (mydata) => {
+    socket.emit(CHEERS, mydata);
+  };
 
   const disconnecting = () => {
     socket.off(CHEERS);
