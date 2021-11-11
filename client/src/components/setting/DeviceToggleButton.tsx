93c3514ad97d291fcdef2b6e8d28784c4f75f52b
Ga-Yeon Park <rkdusdd52@naver.com>
2021-11-11 18:45:32 +0900

    :recycle: 재사용을 위한 컴포넌트 분리

---
+++ client/src/components/setting/DeviceToggleButton.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { XIcon } from '@components/icons';
+import { ToggleButton, IconDiv } from './DeviceToggleButton.style';
+
+type DeviceToggleButtonTypes = {
+  isDeviceOn: boolean;
+  setIsDeviceOn: any;
+  Icon: React.ReactElement | any;
+};
+
+const DeviceToggleButton: React.FC<DeviceToggleButtonTypes> = ({
+  Icon,
+  isDeviceOn,
+  setIsDeviceOn,
+}) => {
+  return (
+    <>
+      <ToggleButton onClick={() => setIsDeviceOn((prev) => !prev)}>
+        <IconDiv>{!isDeviceOn && <XIcon />}</IconDiv>
+        <IconDiv>
+          <Icon />
+        </IconDiv>
+      </ToggleButton>
+    </>
+  );
+};
+
+export default DeviceToggleButton;
