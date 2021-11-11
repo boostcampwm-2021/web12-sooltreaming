0e85235c46c5203568ed5e08020eecdfc2a12a73
pyo-sh <pygosky@gmail.com>
2021-11-11 21:27:58 +0900

    :sparkles: 서버에서 받은 정보를 방 안의 유저에게 제대로 전달할 수 있도록 함

--- server/src/loader/socket.ts
+++ server/src/loader/socket.ts
@@ -4,6 +4,7 @@ import chatting from '@controller/socket/chatting';
 import entering from '@controller/socket/entering';
 import creating from '@controller/socket/creating';
 import animation from '@controller/socket/animation';
+import questionMark from '@controller/socket/questionMark';
 
 import pipe from '@utils/pipe';
 
@@ -39,7 +40,7 @@ const socketLoader = (server, app): any => {
   io.on('connection', (socket: Socket) => {
     console.log('socket connection!!', socket.id);
 
-    pipe(signaling, chatting, creating, entering, animation)({ io, socket, rooms });
+    pipe(signaling, chatting, creating, entering, animation, questionMark)({ io, socket, rooms });
 
     socket.on('disconnect', () => {
       console.log('disconnect socket!!' + socket.id);
