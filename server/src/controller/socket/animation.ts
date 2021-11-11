e44fc4fb77d56f2f3b39cb861e45784f89b26c08
alittlekitten <dlscjs8646@gmail.com>
2021-11-11 17:36:20 +0900

    :bug: 서버에서 방별 애니메이션 처리 구현

--- server/src/controller/socket/animation.ts
+++ server/src/controller/socket/animation.ts
@@ -6,9 +6,11 @@ const CHEERS = 'CHEERS';
 const QUESTION_MARK = 'QUESTION_MARK';
 
 const animation = ({ io, socket, rooms }: { io: any; socket: Socket; rooms: roomType }) => {
-  socket.on(CHEERS, () => {
-    socket.emit(CHEERS, {});
-    socket.broadcast.emit(CHEERS, {});
+  socket.on(CHEERS, ({ chatRoomCode, user }) => {
+    let code = '';
+    code = chatRoomCode;
+    socket.emit(CHEERS);
+    socket.to(code).emit(CHEERS);
   });
   return { io, socket, rooms };
 };
