6cd776f68b9929721deca5f17234b7df97ecbe72
Ga-Yeon Park <rkdusdd52@naver.com>
2021-11-12 00:02:04 +0900

    :recycle: useSetting 커스텀 훅 구현
    PrevSetting과 RoomSetting의 중복코드를 병합하고자 커스텀훅 제작

---
+++ client/src/hooks/useSetting.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import customRTC from '@utils/customRTC';
+import { useRecoilStateLoadable } from 'recoil';
+import { videoState, audioState } from '@src/store/device';
+
+const useSetting = (stream: MediaStream) => {
+  const [videos, setVideos] = useState<MediaDeviceInfo[]>([]);
+  const [audios, setAudios] = useState<MediaDeviceInfo[]>([]);
+  const [selectedVideo, setSelectedVideo] = useRecoilStateLoadable(videoState);
+  const [selectedAudio, setSelectedAudio] = useRecoilStateLoadable(audioState);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const settingMedia = async () => {
+      const videoDevices = await customRTC.getVideos();
+      const audioDevices = await customRTC.getAudios();
+      setIsLoading(false);
+      setVideos(videoDevices);
+      setAudios(audioDevices);
+    };
+    settingMedia();
+  }, []);
+
+  useEffect(() => {
+    const updateVideoStream = async () => {
+      if (isLoading) return;
+      stream.getVideoTracks().forEach((track) => {
+        track.stop();
+        stream.removeTrack(track);
+      });
+      const newVideoTrack = await customRTC.getVideoTrack(selectedVideo.contents.deviceId);
+      if (!newVideoTrack) return;
+      stream.addTrack(newVideoTrack);
+    };
+    updateVideoStream();
+  }, [selectedVideo]);
+
+  useEffect(() => {
+    const updateAudioStream = async () => {
+      if (isLoading) return;
+      stream.getAudioTracks().forEach((track) => {
+        track.stop();
+        stream.removeTrack(track);
+      });
+      const newAudioTrack = await customRTC.getAudioTrack(selectedAudio.contents.deviceId);
+      if (!newAudioTrack) return;
+      stream.addTrack(newAudioTrack);
+    };
+    updateAudioStream();
+  }, [selectedAudio]);
+
+  return {
+    videos,
+    audios,
+    selectedVideo,
+    setSelectedVideo,
+    selectedAudio,
+    setSelectedAudio,
+    isLoading,
+    setIsLoading,
+  };
+};
+
+export default useSetting;
