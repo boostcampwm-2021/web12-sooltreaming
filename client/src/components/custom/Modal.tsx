f74ca639c60c33dc8ed12ca58dacf0c65027015b
pyo-sh <pygosky@gmail.com>
2021-11-16 19:45:02 +0900

    :recycle: custom Modal 수정 (Simple is the Best)

--- client/src/components/custom/Modal.tsx
+++ client/src/components/custom/Modal.tsx
@@ -1,10 +1,5 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { Wrapper, ContentDiv } from './Modal.style';
-
-const MODAL_CONTENT_KEY = 'ModalContent';
-export const ModalContent: React.FC = ({ children }) => {
-  return <div key={MODAL_CONTENT_KEY}>{children}</div>;
-};
 
 export type ModalPosType = {
   top?: number;
@@ -13,45 +8,23 @@ export type ModalPosType = {
   left?: number;
 };
 type ModalPropTypes = {
-  children: ReactNode[];
+  children: any;
   isOpen: boolean;
   relativePos?: ModalPosType;
   absoultePos?: ModalPosType;
 };
 
-const Modal: React.FC<ModalPropTypes> = ({
+const Modal: React.FunctionComponent<ModalPropTypes> = ({
   children,
   isOpen,
   relativePos = {},
   absoultePos = {},
 }) => {
-  if (!children?.length) <></>;
-
-  const [contentChildren, otherChildren] = [...children].reduce(
-    ([conts, others]: Array<ReactNode[]>, cur: ReactNode) => {
-      // Verify Type (Component)
-      const typeFunc = (cur as { type: Function })?.type;
-      const isTargetType =
-        typeFunc && typeof typeFunc === 'function' && typeFunc.name === MODAL_CONTENT_KEY;
-      if (!isTargetType) return [conts, [...others, cur]];
-      // Check Children
-      const grandChildren = (cur as { props: { children: ReactNode | ReactNode[] } })?.props
-        ?.children;
-      if (!grandChildren) return [conts, others];
-      return [[...conts, grandChildren], others];
-    },
-    [[], []],
-  );
-
+  if (!isOpen) return <></>;
   return (
-    <>
-      {otherChildren}
-      {isOpen && (
-        <Wrapper pos={relativePos}>
-          <ContentDiv pos={absoultePos}>{contentChildren}</ContentDiv>
-        </Wrapper>
-      )}
-    </>
+    <Wrapper pos={relativePos}>
+      <ContentDiv pos={absoultePos}>{children}</ContentDiv>
+    </Wrapper>
   );
 };
 
