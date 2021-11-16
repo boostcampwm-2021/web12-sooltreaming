3482be3dcac757b5b9034edf5f46c95ea7dfdd61
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2021-11-16 15:41:09 +0900

    :lipstick: host component CSS 적용

--- client/src/components/chat-room/host/Host.style.js
+++ client/src/components/chat-room/host/Host.style.js
@@ -2,4 +2,11 @@ import styled from 'styled-components';
 import { COLOR } from '@constant/style';
 
 export const Wrapper = styled.div`
+  flex: 1 1 auto;
+  padding: 0 20;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  background-color: ${COLOR.primary2};
+  overflow: hidden;
 `;
