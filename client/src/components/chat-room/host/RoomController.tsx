8330276c209576d584a76c6a0119a2c01bf5dd78
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2021-11-16 21:31:26 +0900

    :sparkles: 방 입장 제한 기능 구현
    토글 버튼으로 방 입장 제한

--- client/src/components/chat-room/host/RoomController.tsx
+++ client/src/components/chat-room/host/RoomController.tsx
@@ -1,24 +1,49 @@
-import React, {useState, useMemo} from 'react'
-import {useParams} from 'react-router-dom';
+import React, { useMemo, useEffect, useRef } from 'react'
+import { useParams } from 'react-router-dom';
 import { FRONT_BASE_URL } from '@src/constant/envs';
 import { CopyIcon } from '@components/icons';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '@src/store';
+
+import { setIsOpen } from '@store/room';
+import {setNoticeMessage} from '@store/notice';
+
+import Socket from '@socket/socket';
 
 import { Wrapper, RowWrapper, IconButton, ToggleButton, DialogButton } from './RoomController.style';
 
 
 const RoomController = () => {
+  const dispatch = useDispatch();
   const { code } = useParams();
-  const [selected, setSelected] = useState(true);
-  
+  const isOpen = useSelector((state: RootState) => state.room.isOpen);
   const roomUrl = useMemo(() => `${FRONT_BASE_URL}/chatRoom/${code}`, [code]);
+  const roomController = useRef<any>(() => {});
+  
+  const errorControl = (message) => {
+    dispatch(setNoticeMessage({ errorMessage: message }));
+  };
 
   const copyURL = () => {
     navigator.clipboard.writeText(roomUrl);
   }
 
-  const toggleSelected = () => {
-    setSelected((prev) => !prev);
+  const requestToggleIsOpen = () => {
+    roomController.current(code);
   }
+
+  const toggleIsOpen = () => {
+    dispatch(setIsOpen({ isOpen: !isOpen }));
+  }
+
+  useEffect(() => {
+    const functions = Socket.roomControl({errorControl, toggleIsOpen});
+    roomController.current = functions.toggleRoomEntry;
+    return () => {
+      functions.disconnecting();
+    };
+  })
 
   return (
     <Wrapper>
@@ -30,9 +55,9 @@ const RoomController = () => {
       </RowWrapper>
       <RowWrapper>
         <span>방 접속 제한 : </span>
-          <ToggleButton onClick={toggleSelected}>
-            <DialogButton isSelected={selected}>
-              {selected ? "Open" : "Close"}
+          <ToggleButton onClick={requestToggleIsOpen}>
+            <DialogButton isSelected={isOpen}>
+              {isOpen ? "Open" : "Close"}
             </DialogButton>
           </ToggleButton>
       </RowWrapper>
