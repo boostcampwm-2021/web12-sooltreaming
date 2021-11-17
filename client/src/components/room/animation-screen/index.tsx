159ef11fd8a6d3868b9c25ad77a5d61fcca84b09
Ssoco <dlscjs8646@gmail.com>
2021-11-18 00:24:06 +0900

    :recycle: 애니메이션 스크린 리팩토링
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
    Co-Authored-By: yeon52 <52206971+yeon52@users.noreply.github.com>
    Co-Authored-By: PyoSeokHun <55688122+pyo-sh@users.noreply.github.com>

--- client/src/components/room/animation-screen/index.tsx
+++ client/src/components/room/animation-screen/index.tsx
@@ -1,29 +1,24 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Wrapper, ScreenImg, QuestionList } from '@components/room/animation-screen/index.style';
 import QuestionMark from './QuestionMark';
-import Socket from '@socket/socket';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '@src/store';
+import { setIsCheers } from '@store/room';
+import useQuestionMark from '@hooks/socket/useQuestionMark';
 
 const CHEERS_GIF_NUM = 2;
 const CHEERS_TIME = 5000;
 const LISTED_GIF = ['/images/beer-cheers1.gif', '/images/beer-cheers2.gif'];
 
-type AnimationScreenPropsType = {
-  isCheers: any;
-  setIsCheers: any;
-  user: object;
-};
-
-type MarkType = {
-  [key: number]: { x: number; y: number };
-};
-
-const AnimationScreen: React.FC<AnimationScreenPropsType> = ({ isCheers, setIsCheers, user }) => {
+const AnimationScreen: React.FC = () => {
+  const dispatch = useDispatch();
   const { code } = useParams();
-  const question = useRef<any>(() => {});
   const screenRef = useRef<HTMLImageElement>(null);
-  const [marks, setMarks] = useState<MarkType>({});
+  const user = useSelector((state: RootState) => state.user);
+  const isCheers = useSelector((state: RootState) => state.room.isCheers);
 
+  const { marks, addQuestionMark } = useQuestionMark();
   // 랜덤한 gif사진을 뽑아서 출력
   const randomDisplay = () => {
     const randomNum = Math.floor(Math.random() * CHEERS_GIF_NUM);
@@ -36,7 +31,7 @@ const AnimationScreen: React.FC<AnimationScreenPropsType> = ({ isCheers, setIsCh
 
     // 5초후에 false로 바꿔서 버튼 동작하게 만듦
     setTimeout(() => {
-      setIsCheers(false);
+      dispatch(setIsCheers(false));
       if (screenRef.current) {
         screenRef.current.src = '';
         screenRef.current.style.display = 'none';
@@ -50,46 +45,17 @@ const AnimationScreen: React.FC<AnimationScreenPropsType> = ({ isCheers, setIsCh
     randomDisplay();
   }, [isCheers]);
 
-  useEffect(() => {
-    const functions = Socket.questionmark({ setMarks });
-    question.current = functions.questionMark;
-    return () => {
-      functions.disconnecting();
-    };
-  }, []);
-
   const onClickScreen = (e) => {
     e.preventDefault();
     const { clientX: x, clientY: y } = e;
-    question.current({
-      x,
-      y,
-      chatRoomCode: code,
-      user,
-    });
-  };
-
-  const disappearSelf = (id) => {
-    setMarks((prev) => {
-      const newMarks = { ...prev };
-      delete newMarks[id];
-      return newMarks;
-    });
+    addQuestionMark({ x, y });
   };
 
   return (
     <Wrapper onContextMenu={onClickScreen}>
       <QuestionList>
         {Object.entries(marks).map(([key, { x, y }]) => {
-          return (
-            <QuestionMark
-              key={`Question-${key}`}
-              identifier={key}
-              disappearSelf={disappearSelf}
-              x={x}
-              y={y}
-            />
-          );
+          return <QuestionMark key={`Question-${key}`} x={x} y={y} />;
         })}
       </QuestionList>
       <ScreenImg ref={screenRef} />
