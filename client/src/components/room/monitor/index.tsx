7a4132fa84e73ee71eff90e73c867c088916c0cd
Ssoco <dlscjs8646@gmail.com>
2021-11-18 00:28:27 +0900

    :recycle: Custom Hook 사용으로 인한 monitor Props 간편화
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
    Co-Authored-By: yeon52 <52206971+yeon52@users.noreply.github.com>
    Co-Authored-By: PyoSeokHun <55688122+pyo-sh@users.noreply.github.com>

--- client/src/components/room/monitor/index.tsx
+++ client/src/components/room/monitor/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Socket from '@socket/socket';
 import { Wrapper, VideoWrapper, Video, Image } from '@components/room/monitor/index.style';
 import { useSelector } from 'react-redux';
@@ -6,34 +6,24 @@ import { RootState } from '@src/store';
 import useUpdateSpeaker from '@hooks/useUpdateSpeaker';
 import useUpdateStream from '@hooks/useUpdateStream';
 import useToggleSpeaker from '@hooks/useToggleSpeaker';
+import useWebRTC from '@hooks/socket/useWebRTC';
 
 type ChatFormPropTypes = {
-  users: any;
   closeupUser: any;
 };
 
-const ChatMonitor: React.FC<ChatFormPropTypes> = ({ users, closeupUser }) => {
-  const socket = useRef<any>(null);
+const ChatMonitor: React.FC<ChatFormPropTypes> = ({ closeupUser }) => {
+  const users = useSelector((state: RootState) => state.room.users);
+  const streams = useSelector((state: RootState) => state.room.streams);
   const stream = useSelector((state: RootState) => state.device.stream);
   const isVideoOn = useSelector((state: RootState) => state.device.isVideoOn);
-  const { id, imgUrl, nickname } = useSelector((state: RootState) => state.user);
-  const [streams, setStreams] = useState<{ [key: string]: MediaStream }>({});
+  const imgUrl = useSelector((state: RootState) => state.user.imgUrl);
   const myVideoRef = useRef<HTMLVideoElement>(null);
   const className = closeupUser ? (Socket.getSID() === closeupUser ? 'closeup' : 'mini') : '';
   let count = Object.values(streams).length + 1;
 
-  useEffect(() => {
-    // Socket으로 Peer Connection 만들기
-    const webRTCSocket = Socket.webRTC({ setStreams, stream });
-    socket.current = webRTCSocket;
-    return () => {
-      webRTCSocket.disconnecting();
-    };
-  }, []);
-
-  const sendStream = () => {
-    socket.current?.changeStream(stream);
-  };
+  const { changeStream } = useWebRTC();
+  const sendStream = () => changeStream(stream);
   useUpdateStream(myVideoRef, stream, sendStream);
 
   return (
@@ -46,27 +36,24 @@ const ChatMonitor: React.FC<ChatFormPropTypes> = ({ users, closeupUser }) => {
       {Object.entries(streams).map(([sid, otherStream]) => {
         const peerClassName = closeupUser ? (sid === closeupUser ? 'closeup' : 'mini') : '';
         return (
-          <OtherVideo
-            count={count}
-            className={peerClassName}
-            srcObject={otherStream}
-            users={users}
-            sid={sid}
-          />
+          <OtherVideo count={count} className={peerClassName} srcObject={otherStream} sid={sid} />
         );
       })}
     </Wrapper>
   );
 };
 
-const OtherVideo = ({ className, srcObject, count, users, sid }) => {
+const OtherVideo = ({ className, srcObject, count, sid }) => {
+  const users = useSelector((state: RootState) => state.room.users);
+  const usersDevices = useSelector((state: RootState) => state.room.usersDevices);
+
   const otherRef = useRef<HTMLVideoElement>(null);
   useUpdateSpeaker(otherRef);
   useToggleSpeaker(otherRef);
   useUpdateStream(otherRef, srcObject); // srcObject은 otherStream
-  let isVideoOn = users[sid].isVideoOn;
+  let isVideoOn = usersDevices[sid].isVideoOn;
   let imgUrl = users[sid].imgUrl;
-  // map으로 하나씩 보내줄 떄 오는 sid: otherStream의 isVideoOn 정보는 users[sid].isVideoOn가 담고있지 않을까????
+  // map으로 하나씩 보내줄 떄 오는 sid: otherStream의 isVideoOn 정보는 usersDevices[sid].isVideoOn가 담고있지 않을까????
 
   return (
     <>
