d1c67d343472c3921ec6c38b3f30102c2a96e959
Ssoco <dlscjs8646@gmail.com>
2021-11-18 00:22:46 +0900

    :sparkles: socket과 상태를 연결시키는 useUser Custom Hook 추가
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
    Co-Authored-By: yeon52 <52206971+yeon52@users.noreply.github.com>
    Co-Authored-By: PyoSeokHun <55688122+pyo-sh@users.noreply.github.com>

---
+++ client/src/hooks/socket/useUser.ts
@@ -0,0 +1,64 @@
+import { useMemo, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '@src/store';
+import { setUsers, addUsers, deleteUsers, setHost } from '@store/room';
+import { setNoticeMessage } from '@store/notice';
+import Socket from '@socket/socket';
+
+const useUser = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => {
+    const { id, imgUrl, nickname } = state.user;
+    return { id, imgUrl, nickname };
+  });
+  const code = useSelector((state: RootState) => state.room.roomCode);
+  const isVideoOn = useSelector((state: RootState) => state.device.isVideoOn);
+
+  const errorControl = (message) => {
+    dispatch(setNoticeMessage({ errorMessage: message }));
+    history.push('/');
+  };
+
+  const addUser = ({ user, userDevices, sid }) => {
+    dispatch(addUsers({ user, userDevices, sid }));
+  };
+
+  const deleteUser = (sid) => {
+    dispatch(deleteUsers(sid));
+  };
+
+  const initUsers = (data) => {
+    dispatch(setUsers(data));
+  };
+
+  const changeRoomHost = (hostId, isOpen) => {
+    dispatch(setHost({ hostId, isOpen }));
+  };
+
+  const socket = useMemo(
+    () =>
+      Socket.user({
+        errorControl,
+        addUser,
+        deleteUser,
+        initUsers,
+        changeRoomHost,
+      }),
+    [],
+  );
+  useEffect(() => {
+    socket.joinRoom({
+      chatRoomCode: code,
+      user,
+      isVideoOn,
+    });
+    return () => {
+      socket.disconnecting();
+    };
+  }, []);
+  return socket;
+};
+
+export default useUser;
