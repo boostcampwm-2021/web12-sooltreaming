af484e8d24c6fc91e53746ca83deec41a494073f
Ssoco <dlscjs8646@gmail.com>
2021-11-18 00:28:03 +0900

    :recycle: Custom Hook 사용으로 인한 room index Props 간편화
    완전 예쁨
    
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
    Co-Authored-By: yeon52 <52206971+yeon52@users.noreply.github.com>
    Co-Authored-By: PyoSeokHun <55688122+pyo-sh@users.noreply.github.com>

--- client/src/components/room/index.tsx
+++ client/src/components/room/index.tsx
@@ -1,107 +1,35 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '@src/store';
-import { setNoticeMessage } from '@store/notice';
-import { resetRoomInfo, setHost } from '@store/room';
-import Socket from '@socket/socket';
+import React, { useEffect } from 'react';
 import RoomMenu from '@components/room/RoomMenu';
 import ChatMonitor from '@components/room/monitor/';
 import ControlBar from '@components/room/ControlBar';
 import { Wrapper, VideoSection, ColumnDiv } from '@components/room/index.style';
 import AnimationScreen from '@components/room/animation-screen/';
 import Scaffold from '@components/room/scaffold';
+import useUser from '@hooks/socket/useUser';
+import useAnimation from '@hooks/socket/useAnimation';
+import Socket from '@socket/socket';
 
 const ChatRoom: React.FC = () => {
-  const dispatch = useDispatch();
-  const activateCheers = useRef<any>(() => {});
-  const activateCloseup = useRef<any>(() => {});
-  const deactivateCloseup = useRef<any>(() => {});
-  const history = useHistory();
-  const { code } = useParams();
-
-  const { id, imgUrl, nickname } = useSelector((state: RootState) => state.user);
-  const user = { id, imgUrl, nickname };
-  const [users, setUsers] = useState({});
-  const [menuType, setMenuType] = useState<string>('채팅');
-  const [isCheers, setIsCheers] = useState<boolean>(false);
-  const { isVideoOn } = useSelector((state: RootState) => state.device);
-
-  const [closeupUser, setCloseupUser] = useState<string>('');
-  const errorControl = (message) => {
-    dispatch(setNoticeMessage({ errorMessage: message }));
-    history.push('/');
-  };
-
-  const changeRoomHost = (hostId, isOpen) => {
-    dispatch(setHost({ hostId, isOpen }));
-  };
-
   useEffect(() => {
     Socket.connect();
-    const functions = Socket.user({ errorControl, setUsers, changeRoomHost });
-    functions.joinRoom({
-      chatRoomCode: code,
-      user,
-      isVideoOn,
-    });
-  }, []);
-
-  useEffect(() => {
-    const functions = Socket.animation({ setIsCheers, setCloseupUser });
-    activateCheers.current = functions.activateCheers;
-    activateCloseup.current = functions.activateCloseup;
-    deactivateCloseup.current = functions.deactivateCloseup;
     return () => {
-      functions.disconnecting();
-      dispatch(resetRoomInfo({}));
+      Socket.disconnect();
     };
   }, []);
 
-  useEffect(() => {
-    const functions = Socket.isVideoOnOff({ errorControl, users, setUsers });
-    functions.videoChange({
-      sid: Socket.getSID(),
-      chatRoomCode: code,
-      nowVideoOn: isVideoOn,
-    });
-    return () => {
-      functions.disconnecting();
-    };
-  }, [isVideoOn]);
-
-  const cheers = (e) => {
-    if (isCheers) return;
-    activateCheers.current({
-      chatRoomCode: code,
-      user,
-    });
-  };
-
-  const closeup = (e) => {
-    if (closeupUser) {
-      deactivateCloseup.current({
-        chatRoomCode: code,
-        closeupUser,
-      });
-    } else {
-      activateCloseup.current({
-        chatRoomCode: code,
-        sid: Socket.getSID(),
-      });
-    }
-  };
+  useUser();
+  const { cheers, closeup, closeupUser } = useAnimation();
 
   return (
     <Wrapper>
       <ColumnDiv>
         <VideoSection>
-          <ChatMonitor users={users} closeupUser={closeupUser} />
-          <AnimationScreen isCheers={isCheers} setIsCheers={setIsCheers} user={user} />
+          <ChatMonitor closeupUser={closeupUser} />
+          <AnimationScreen />
         </VideoSection>
-        <ControlBar onClickCheers={cheers} onClickCloseup={closeup} setMenuType={setMenuType} />
+        <ControlBar onClickCheers={cheers} onClickCloseup={closeup} />
       </ColumnDiv>
-      <RoomMenu menuType={menuType} setMenuType={setMenuType} user={user} users={users} />
+      <RoomMenu />
       <Scaffold />
     </Wrapper>
   );
