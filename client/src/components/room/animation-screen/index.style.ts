0e43b1637fa5906054fd5340079b12edf0575b37
Ssoco <dlscjs8646@gmail.com>
2021-11-18 00:02:39 +0900

    :lipstick: 건배 Z_INDEX 수정
    어머나! 넷째에게 먹혀버린 셋째
    
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
    Co-Authored-By: yeon52 <52206971+yeon52@users.noreply.github.com>
    Co-Authored-By: PyoSeokHun <55688122+pyo-sh@users.noreply.github.com>

--- client/src/components/room/animation-screen/index.style.ts
+++ client/src/components/room/animation-screen/index.style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Z_INDEX } from '@constant/style';
 
 export const Wrapper = styled.div`
   position: relative;
@@ -6,7 +7,7 @@ export const Wrapper = styled.div`
   height: 100%;
   top: -100%;
 
-  z-index: 2;
+  z-index: ${Z_INDEX.cheers};
 `;
 
 export const ScreenImg = styled.img`
