7535b59d77ecda73a22470c1baa94d27c6f55a85
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2021-11-18 16:57:52 +0900

    :recycle: 이벤트 명 변경
    동사가 앞으로!

--- server/src/controller/socket/restricting.ts
+++ server/src/controller/socket/restricting.ts
@@ -6,8 +6,8 @@ const TURN_OFF_OTHER_VIDEO = 'TURN_OFF_OTHER_VIDEO';
 const TURN_OFF_OTHER_AUDIO = 'TURN_OFF_OTHER_AUDIO';
 const TOGGLE_ROOM_ENTRY = 'TOGGLE_ROOM_ENTRY';
 const AUTHORITY_ERROR = 'AUTHORITY_ERROR';
-const VIDEO_CHANGE = 'VIDEO_CHANGE';
-const AUDIO_CHANGE = 'AUDIO_CHANGE';
+const CHANGE_VIDEO = 'CHANGE_VIDEO';
+const CHANGE_AUDIO = 'CHANGE_AUDIO';
 
 const restricting = ({
   io,
@@ -37,7 +37,7 @@ const restricting = ({
       return socket.emit(AUTHORITY_ERROR, '당신은 방장이 아닙니다.');
     const targetRoom = rooms[code];
     targetRoom.usersDevices[sid] = { ...targetRoom.usersDevices[sid], isVideoOn };
-    io.to(code).emit(VIDEO_CHANGE, { sid, isVideoOn });
+    io.to(code).emit(CHANGE_VIDEO, { sid, isVideoOn });
   });
 
   socket.on(TURN_OFF_OTHER_AUDIO, ({sid, isAudioOn}) => {
@@ -45,7 +45,7 @@ const restricting = ({
 
     if (rooms[code].hostID !== socket.id)
       return socket.emit(AUTHORITY_ERROR, '당신은 방장이 아닙니다.');
-    io.to(sid).emit(AUDIO_CHANGE, { isAudioOn });
+    io.to(sid).emit(CHANGE_AUDIO, { isAudioOn });
   });
 
   return { io, socket, rooms };
