ad2cb0d461ea9f12d1f15b628eb2fa214e455803
Ssoco <dlscjs8646@gmail.com>
2021-11-19 00:17:38 +0900

    :sparkles: 내정보 페이지 구현
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>

--- client/src/components/user-information/Information.style.js
+++ client/src/components/user-information/Information.style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { COLOR } from '@constant/style';
+import { COLOR, INPUT_STYLE } from '@constant/style';
 
 export const TopWrapper = styled.div`
   display: flex;
@@ -91,4 +91,120 @@ export const BottomWrapper = styled.div`
     height: 30px;
     margin: 0.25em 0;
   }
+`;
+
+export const InformationSpan = styled.span`
+  font-weight: bold;
+`;
+
+export const Header = styled.div`
+  padding: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > h2 {
+    padding: 0;
+    margin: 30px 0 0 15px;
+    color: ${COLOR.titleActive};
+    user-select: none;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  & span {
+    color: ${COLOR.point};
+  }
+`;
+
+export const DeleteChangePressSection = styled.div`
+  width: 640px;
+  padding: 50px 70px;
+  margin-bottom: 30px;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  & > button {
+    width: 180px;
+
+    outline: none;
+    background-color: ${COLOR.white};
+    border-radius: 10px;
+    border: 0px solid ${COLOR.line};
+    cursor: pointer;
+  }
+  & img {
+    width: 100%;
+    height: 100%;
+
+    -webkit-user-drag: none;
+  }
+`;
+
+export const CheckPressSection = styled.div`
+  width: 640px;
+  padding: 30px 30px;
+  margin-bottom: 10px;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  & > button {
+    width: 200px;
+
+    outline: none;
+    background-color: ${COLOR.white};
+    border-radius: 10px;
+    border: 0px solid ${COLOR.line};
+    cursor: pointer;
+  }
+
+  & img {
+    width: 100%;
+    height: 100%;
+
+    -webkit-user-drag: none;
+  }
+`;
+
+export const HistoryData = styled.div`
+  width: 640px;
+  padding: 20px 20px;
+  display: flex;
+  flex-direction: column;
+
+  justify-content: space-around;
+  align-items: center;
+
+  p {
+    margin: 8px;
+  }
+`;
+
+export const ChangeNicknameData = styled.div`
+  width: 640px;
+  padding: 20px 20px;
+  display: flex;
+  flex-direction: column;
+
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const NewNicknameInput = styled.input`
+  ${INPUT_STYLE}
+  width: 100%;
+  max-width: 500px;
+  height: 60px;
+  padding: 0 30px;
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  border-radius: 100px;
+  font-weight: 500;
+  font-size: 24px;
+  text-align: center;
 `;
