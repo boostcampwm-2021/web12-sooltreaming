15a69e02849ed6ce931516f7073769619510b44e
Ssoco <dlscjs8646@gmail.com>
2021-11-19 00:12:03 +0900

    :sparkles: QuestionMark DB에 로그 저장
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>

--- server/src/controller/socket/questionMark.ts
+++ server/src/controller/socket/questionMark.ts
@@ -1,6 +1,7 @@
 import { Socket } from 'socket.io';
 import type { roomType } from '@loader/socket';
 import type { TargetInfoType } from '@controller/socket/entering';
+import { createLog } from '@controller/socket/logController';
 
 const QUESTION = 'QUESTION';
 
@@ -17,7 +18,10 @@ const questionMark = ({
 }) => {
   socket.on(QUESTION, ({ x, y }) => {
     const { code } = targetInfo;
+
     io.to(code).emit(QUESTION, { x, y });
+    const id = rooms[code].users[socket.id].id;
+    createLog(id, QUESTION);
   });
   return { io, socket, rooms, targetInfo };
 };
