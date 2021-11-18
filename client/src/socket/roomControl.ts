7b5f7bb065316126a0ea8d1826f1672cd240d30f
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2021-11-18 15:40:42 +0900

    :sparkles: 특정 사용자 비디오 OFF front socket event 생성

--- client/src/socket/roomControl.ts
+++ client/src/socket/roomControl.ts
@@ -2,6 +2,7 @@ import { Socket } from 'socket.io-client';
 
 const AUTHORITY_ERROR = 'AUTHORITY_ERROR';
 const TOGGLE_ROOM_ENTRY = 'TOGGLE_ROOM_ENTRY';
+const TURN_OFF_OTHER_VIDEO = 'TURN_OFF_OTHER_VIDEO';
 
 const roomControl = (socket: Socket) => (closure: any) => {
   const { errorControl, changeIsOpen } = closure;
@@ -19,11 +20,14 @@ const roomControl = (socket: Socket) => (closure: any) => {
     socket.emit(TOGGLE_ROOM_ENTRY);
   };
 
+  const turnOffOtherVideo = ({ sid, isVideoOn }) => {
+    socket.emit(TURN_OFF_OTHER_VIDEO, { sid, isVideoOn });
+  }
   const disconnecting = () => {
     socket.off(TOGGLE_ROOM_ENTRY);
   };
 
-  return { toggleRoomEntry, disconnecting };
+  return { toggleRoomEntry, turnOffOtherVideo, disconnecting };
 };
 
 export default roomControl;
