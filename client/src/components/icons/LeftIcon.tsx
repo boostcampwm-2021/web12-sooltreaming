ec4941e6cd009644524ea22771885e287c9aca76
Ssoco <dlscjs8646@gmail.com>
2021-11-19 01:18:55 +0900

    :bug: svg 파일 {} 추가

--- client/src/components/icons/LeftIcon.tsx
+++ client/src/components/icons/LeftIcon.tsx
@@ -9,11 +9,17 @@ const LeftIcon = ({
   stroke,
 }: iconPropsType): React.ReactElement => {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width={width ?? 24}
+      height={height ?? 24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
         d="M16 20L8 12L16 4"
-        stroke="#222222"
-        stroke-width="2"
+        stroke={stroke ?? '#222222'}
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
