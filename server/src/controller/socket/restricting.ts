f5ce60533e3f4f964411de195765da5b7c55d925
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2021-11-18 15:38:37 +0900

    :sparkles: 특정 사용자 비디오 OFF backend event 생성

--- server/src/controller/socket/restricting.ts
+++ server/src/controller/socket/restricting.ts
@@ -2,8 +2,10 @@ import { Socket } from 'socket.io';
 import type { roomType } from '@loader/socket';
 import type { TargetInfoType } from '@controller/socket/entering';
 
+const TURN_OFF_OTHER_VIDEO = 'TURN_OFF_OTHER_VIDEO';
 const TOGGLE_ROOM_ENTRY = 'TOGGLE_ROOM_ENTRY';
 const AUTHORITY_ERROR = 'AUTHORITY_ERROR';
+const VIDEO_CHANGE = 'VIDEO_CHANGE';
 
 const restricting = ({
   io,
@@ -26,6 +28,15 @@ const restricting = ({
     socket.emit(TOGGLE_ROOM_ENTRY, true);
   });
 
+  socket.on(TURN_OFF_OTHER_VIDEO, ({sid, isVideoOn}) => {
+    const { code } = targetInfo;
+
+    if (rooms[code].hostID !== socket.id)
+      return socket.emit(AUTHORITY_ERROR, '당신은 방장이 아닙니다.');
+    const targetRoom = rooms[code];
+    targetRoom.usersDevices[sid] = { ...targetRoom.usersDevices[sid], isVideoOn };
+    io.to(code).emit(VIDEO_CHANGE, { sid, isVideoOn });
+  });
   return { io, socket, rooms };
 };
 
