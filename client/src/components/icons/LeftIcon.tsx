a1ab47c0d4dfbd29000346e766a9ce18a983ab56
Ssoco <dlscjs8646@gmail.com>
2021-11-18 14:39:17 +0900

    :lipstick: 뒤로가기 버튼 등록

---
+++ client/src/components/icons/LeftIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import type { iconPropsType } from '@components/icons';
+
+const LeftIcon = ({
+  className,
+  width,
+  height,
+  fill,
+  stroke,
+}: iconPropsType): React.ReactElement => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M16 20L8 12L16 4"
+        stroke="#222222"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default LeftIcon;
