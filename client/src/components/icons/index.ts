d04261d8bb8948e97a9fc77a84bd079da15fd487
pyo-sh <pygosky@gmail.com>
2021-11-18 17:29:51 +0900

    :sparkles: 투표자 아이콘 추가

--- client/src/components/icons/index.ts
+++ client/src/components/icons/index.ts
@@ -5,6 +5,7 @@ import MicIcon from '@components/icons/MicIcon';
 import VideoIcon from '@components/icons/VideoIcon';
 import SpeakerIcon from '@components/icons/SpeakerIcon';
 import XIcon from '@components/icons/XIcon';
+import VoterIcon from '@components/icons/VoterIcon';
 
 import CheersIcon from '@components/icons/CheersIcon';
 import CloseUpIcon from '@components/icons/CloseUpIcon';
@@ -33,6 +34,7 @@ export {
   VideoIcon,
   SpeakerIcon,
   XIcon,
+  VoterIcon,
   ChatIcon,
   CheersIcon,
   CloseUpIcon,
@@ -40,5 +42,5 @@ export {
   PeopleIcon,
   HostIcon,
   GameIcon,
-  CopyIcon
+  CopyIcon,
 };
