e83631678ba882eeb38e5c9fba96473162ef8bac
Ssoco <dlscjs8646@gmail.com>
2021-11-19 00:08:53 +0900

    :sparkles: getUserNicknameLog API 추가
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>

--- server/src/api/v1/user.ts
+++ server/src/api/v1/user.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import {
+  getUserInformation,
+  getUserNicknameLog,
+} from '@controller/user/user';
 const router = express.Router();
 
 router.get('/', getUserInformation);
+router.get('/nickname', getUserNicknameLog);
 
 export default router;
--- server/src/controller/user/user.ts
+++ server/src/controller/user/user.ts
@@ -1,5 +1,6 @@
 import { CustomError, errorHandler } from '@utils/error';
 import User from '@models/User';
+import NicknameLog from '@models/NicknameLog';
 export const getUserInformation = async (req, res, next) => {
   const { id } = req.query;
   try {
@@ -25,3 +26,24 @@ export const getUserInformation = async (req, res, next) => {
     next(errorHandler(error));
   }
 };
+
+export const getUserNicknameLog = async (req, res, next) => {
+  const id = req.user._id || '618bf4a7e80ad781c6ae8bdd';
+  try {
+    if (!id) throw new CustomError(401, 'id Error');
+    const nicknameLog = await NicknameLog.find({ userId: id })
+      .sort({ createdAt: 'desc' })
+      .select('nickname createdAt')
+      .limit(5);
+
+    if (!nicknameLog) throw new CustomError(400, 'id Error');
+
+    res.status(200).json({
+      nicknameLog,
+      message: 'User Information Update Success',
+    });
+  } catch (error) {
+    next(errorHandler(error));
+  }
+};
+
