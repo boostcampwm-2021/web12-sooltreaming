68e090aaeec1044aa925f7ad6e1215798b07d4ab
pyo-sh <pygosky@gmail.com>
2021-11-23 00:16:37 +0900

    :recycle: 애니메이션 훅 부분 주석 제거

--- client/src/hooks/socket/useAnimation.ts
+++ client/src/hooks/socket/useAnimation.ts
@@ -1,12 +1,10 @@
-import { useMemo, useCallback, useEffect, useRef, useState } from 'react';
+import { useMemo, useCallback, useEffect } from 'react';
 import Socket from '@socket/socket';
 import { useDispatch } from 'react-redux';
 import { setIsCheers, setCloseUpUser, resetRoomInfo } from '@store/room';
-import { RootState } from '@src/store';
 
 const useAnimation = () => {
   const dispatch = useDispatch();
-  const [closeupUser, setCloseupUser] = useState<string>('');
 
   const updateCheers = useCallback((data) => {
     dispatch(setIsCheers(data));
@@ -16,20 +14,8 @@ const useAnimation = () => {
     dispatch(setCloseUpUser(data));
   }, []);
 
-  // const closeup = (e) => {
-  //   if (closeupUser) {
-  //     deactivateCloseup.current();
-  //   } else {
-  //     console.log(e);
-  //     activateCloseup.current();
-  //   }
-  // };
   const socket = useMemo(() => Socket.animation({ updateCheers, updateCloseUpUser }), []);
   useEffect(() => {
-    // activateCheers.current = socket.activateCheers;
-    // activateCloseup.current = socket.activateCloseup;
-    // deactivateCloseup.current = socket.deactivateCloseup;
-
     return () => {
       socket.disconnecting();
       dispatch(resetRoomInfo({}));
