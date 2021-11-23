308121de3a53217f0f6ca572cfc82112046b36a7
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2021-11-23 10:11:02 +0900

    :lipstick: 분리된 컴포넌트 각 스타일 적용

---
+++ client/src/components/user-information/FriendItem.style.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import { COLOR, BTN_STYLE, CANCLE_BTN_STYLE } from '@constant/style';
+
+export const Friend = styled.div`
+  display: inline-flex;
+  width: 250px;
+  height: 60px;
+  align-items: center;
+  justify-content: space-between;
+  margin: 8px;
+  padding: 0 20px;
+  border: 1px solid ${COLOR.primary1};
+
+  .left-items {
+    display: inline-flex;
+    align-items: center;
+    overflow: hidden;
+
+    img {
+      width: 1.5rem;
+      height: 1.5rem;
+      margin-right: 1rem;
+      border-radius: 5px;
+    }
+  }
+
+  .right-items {
+    overflow: hidden;
+    cursor: pointer;
+
+    button{
+      width: 60px;
+      heigth: 20px;
+      border-radius: 5px;
+    }
+
+    .cancle-button {
+      ${CANCLE_BTN_STYLE}
+    }
+
+    .add-button {
+      ${BTN_STYLE}
+    }
+
+    .home-button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      width: 1.5rem;
+      height: 1.5rem;
+      margin-right: 1rem;
+      border-radius: 5px;
+
+      ${BTN_STYLE}
+    }
+  }
+`;
