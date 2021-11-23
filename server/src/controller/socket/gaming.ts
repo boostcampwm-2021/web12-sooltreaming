9f57123faa87a03f6d0e89ed45cd16201d037160
yeon52 <52206971+yeon52@users.noreply.github.com>
2021-11-23 17:16:50 +0900

    :recycle: 게임 실행이 불가능한 경우를 서버에서 처리
    게임이 진행중이거나 투표가 진행중일때 게임 실행

--- server/src/controller/socket/gaming.ts
+++ server/src/controller/socket/gaming.ts
@@ -4,6 +4,8 @@ import type { TargetInfoType } from '@controller/socket/entering';
 
 const START_UPDOWN = 'START_UPDOWN';
 const STOP_UPDOWN = 'STOP_UPDOWN';
+const STATUS_NORMAL = 'STATUS_NORMAL';
+
 const gaming = ({
   io,
   socket,
@@ -16,9 +18,22 @@ const gaming = ({
   targetInfo: TargetInfoType;
 }) => {
   socket.on(START_UPDOWN, (startingSID) => {
+    const { code } = targetInfo;
+    if (!(code in rooms)) return;
+
+    const userKeys = Object.keys(rooms[code].users);
+    if (userKeys.length < 2 || rooms[code].game.title || rooms[code].status !== STATUS_NORMAL)
+      return;
+
+    rooms[code].game = { title: '업다운', host: startingSID };
     io.emit(START_UPDOWN, startingSID);
   });
+
   socket.on(STOP_UPDOWN, () => {
+    const { code } = targetInfo;
+    if (!(code in rooms)) return;
+
+    rooms[code].game = { title: '', host: '' };
     io.emit(STOP_UPDOWN);
   });
   return { io, socket, rooms, targetInfo };
