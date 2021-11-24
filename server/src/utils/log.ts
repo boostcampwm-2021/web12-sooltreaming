38aef21a1dcb06c999d3ee22d47893fc77f562ed
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2021-11-24 22:08:48 +0900

    :recycle:  logController 파일명 변경 및 이동

--- server/src/utils/log.ts
+++ server/src/utils/log.ts
@@ -2,5 +2,5 @@ import User from '@models/User';
 import { LOG_EVENT } from '@src/constant';
 
 export const createLog = async (id, eventType) => {
-  await User.updateOne({ _id: id }, { $inc: { [LOG_EVENT[eventType]]: 1 } });
+  const user = await User.updateOne({ _id: id }, { $inc: { [LOG_EVENT[eventType]]: 1 } });
 };
