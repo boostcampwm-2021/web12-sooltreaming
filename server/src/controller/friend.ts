9d3eb326adb569573c46ff9182c71bddc9a00529
yeon52 <52206971+yeon52@users.noreply.github.com>
2021-11-25 23:39:29 +0900

    :sparkles: 친구 신청, 수락 예외처리
    동시에 수락, 취소하거나 동시에 친구신청 시 서버에서 오류 메시지 전송 및 예외처리
    
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
    Co-Authored-By: PyoSeokHun <55688122+pyo-sh@users.noreply.github.com>
    Co-Authored-By: Ssoco <dlscjs8646@gmail.com>

--- server/src/controller/friend.ts
+++ server/src/controller/friend.ts
@@ -4,6 +4,13 @@ import User from '@models/User';
 export const postFriend = errorWrapper(async (req, res, next) => {
   const { targetId } = req.body;
   const { _id } = JSON.parse(JSON.stringify(req.user));
+
+  const { receiveFriend: myReceiveFriend } = await User.findOne({ _id }).select(
+    'receiveFriend -_id',
+  );
+  if (myReceiveFriend.includes(targetId))
+    throw new CustomError(400, '♡ 상대방이 이미 칭구 걸었지용가리 ^0^ ♡');
+
   await User.updateOne({ _id }, { $addToSet: { sendFriend: targetId } });
   await User.updateOne({ _id: targetId }, { $addToSet: { receiveFriend: _id } });
   res.status(201).json({ message: 'Request Friend Success' });
@@ -61,6 +68,13 @@ export const patchUnfriend = errorWrapper(async (req, res, next) => {
 export const patchFriend = errorWrapper(async (req, res, next) => {
   const { targetId } = req.body;
   const { _id } = JSON.parse(JSON.stringify(req.user));
+
+  const { receiveFriend: myReceiveFriend } = await User.findOne({ _id }).select(
+    'receiveFriend -_id',
+  );
+  if (!myReceiveFriend.includes(targetId))
+    throw new CustomError(400, ' 엥 님 손 절 당했을 지 도, ,., . ?');
+
   await User.updateOne(
     { _id },
     { $pull: { receiveFriend: { $in: [targetId] } }, $addToSet: { friend: targetId } },
