55f2ffd43d39494811a81965b5933cfa5271be0a
yeon52 <52206971+yeon52@users.noreply.github.com>
2021-11-25 10:54:07 +0900

    :lipstick: 참가자 리스트 스크롤바 추가

--- client/src/components/user/Users.style.js
+++ client/src/components/user/Users.style.js
@@ -7,7 +7,17 @@ export const MenuBox = styled.div`
   display: flex;
   flex-direction: column;
   background-color: ${COLOR.primary2};
-  overflow: hidden;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 4px;
+    height: 16px;
+    border-radius: 10px;
+    background: ${COLOR.line};
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${COLOR.primary3};
+    border-radius: 10px;
+  }
 `;
 
 export const UserList = styled.li`
