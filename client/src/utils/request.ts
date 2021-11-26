192f584ab5b14aab42974a978652ca31eec3adaf
pyo-sh <pygosky@gmail.com>
2021-11-26 18:52:51 +0900

    :bug: 이미지 전송을 위해 FormData 사용으로 인해 custom Fetch를 사용하지 못하던 버그 수정
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
    Co-Authored-By: Ssoco <dlscjs8646@gmail.com>
    Co-Authored-By: yeon52 <52206971+yeon52@users.noreply.github.com>

--- client/src/utils/request.ts
+++ client/src/utils/request.ts
@@ -5,14 +5,14 @@ const BASE_URL = `${BACK_BASE_URL}/api/${BACK_VERSION}`;
 interface fetchParams {
   url: string;
   query?: { [key: string]: string };
-  body?: Object;
+  body?: FormData | Object;
   headerOptions?: HeadersInit;
   options?: RequestInit;
 }
 
 const customFetch =
   (method: string) =>
-  async ({ url, query, body, headerOptions, options }: fetchParams) => {
+  async ({ url, query, body = {}, headerOptions, options }: fetchParams) => {
     const query_string = query
       ? `?${Object.entries(query)
           .reduce((prev, [key, value]) => [...prev, `${key}=${value}`], [] as Array<string>)
@@ -24,11 +24,18 @@ const customFetch =
       method,
       credentials: 'include' as RequestCredentials,
       headers: {
-        ...(headerOptions ?? {}),
-        'Content-Type': 'application/json',
+        ...(headerOptions ?? { 'Content-Type': 'application/json' }),
       },
     };
-    if (method === 'POST' || method === 'PATCH') init['body'] = JSON.stringify(body ?? {});
+
+    if (method !== 'GET') {
+      if (body.toString() === '[object FormData]') {
+        init.body = body as FormData;
+      } else {
+        init.body = JSON.stringify(body);
+      }
+    }
+
     const resolve = await fetch(`${BASE_URL}${url}${query_string}`, init);
     const { status } = resolve;
     const json = await resolve.json();
@@ -39,6 +46,7 @@ const request = {
   get: customFetch('GET'),
   post: customFetch('POST'),
   patch: customFetch('PATCH'),
+  delete: customFetch('DELETE'),
 };
 
 export default request;
