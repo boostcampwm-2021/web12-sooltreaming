d482538c490606f41766e8861fa7dfd433934246
Ssoco <dlscjs8646@gmail.com>
2021-12-01 00:51:45 +0900

    :bug: 타입 이름 빼먹은 부분 추가

--- client/src/components/icons/AcceptIcon.tsx
+++ client/src/components/icons/AcceptIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { iconPropType } from '@ts-types/components/icons';
+import type { IconPropType } from '@ts-types/components/icons';
 
 const AcceptIcon = ({
   className,
@@ -7,7 +7,7 @@ const AcceptIcon = ({
   height,
   fill,
   stroke,
-}: iconPropType): React.ReactElement => {
+}: IconPropType): React.ReactElement => {
   return (
     <svg
       width={width ?? 136}
