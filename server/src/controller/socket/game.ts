7b083834c0eedb0284359d82868be73a68562d36
Ssoco <dlscjs8646@gmail.com>
2021-12-01 05:06:11 +0900

    :sparkles: 서버에서 라이어게임을 위한 소켓을 생성 및 이벤트 받는 부분 일부 변경
    이벤트명을 추가하고, GAME_STOP 이벤트를 받도록 수정

--- server/src/controller/socket/game.ts
+++ server/src/controller/socket/game.ts
@@ -1,7 +1,7 @@
 import { createLog } from '@service/user';
-import { UPDOWN_START, UPDOWN_STOP } from 'sooltreaming-domain/constant/socketEvent';
-import { STATUS_VOTE_NORMAL } from '@src/constant';
-import { UP_DOWN } from 'sooltreaming-domain/constant/gameName';
+import { UPDOWN_START, LIAR_START, GAME_STOP } from 'sooltreaming-domain/constant/socketEvent';
+import { STATUS_VOTE_NORMAL, KEYWORDS, LIAR_KEYWORD } from '@src/constant';
+import { UP_DOWN, LIAR } from 'sooltreaming-domain/constant/gameName';
 import type { SocketPropType } from '@src/types';
 
 const game = ({ io, socket, rooms, targetInfo }: SocketPropType): SocketPropType => {
@@ -19,7 +19,27 @@ const game = ({ io, socket, rooms, targetInfo }: SocketPropType): SocketPropType
     io.to(code).emit(UPDOWN_START, startingSID, randomNum);
     createLog(rooms[code].users[startingSID].id, UPDOWN_START);
   });
-  socket.on(UPDOWN_STOP, () => {
+
+  socket.on(LIAR_START, (startingSID) => {
+    const { code } = targetInfo;
+    if (!(code in rooms)) return;
+
+    const userKeys = Object.keys(rooms[code].users);
+    if (userKeys.length < 2 || rooms[code].game.title || rooms[code].status !== STATUS_VOTE_NORMAL)
+      return;
+
+    rooms[code].game = { title: LIAR, host: startingSID };
+    const usersSID = Object.keys(rooms[code].users); // 방 안의 유저들의 sid들을 모아놓은 string 배열
+    const randomSID = usersSID[Math.floor(Math.random() * Object.keys(rooms[code].users).length)];
+    const randomKeyword = KEYWORDS[Math.floor(Math.random() * KEYWORDS.length)];
+
+    usersSID.forEach((elem) => {
+      if (elem === randomSID) io.to(elem).emit(LIAR_START, startingSID, LIAR_KEYWORD);
+      else io.to(elem).emit(LIAR_START, startingSID, randomKeyword);
+    });
+  });
+
+  socket.on(GAME_STOP, () => {
     const { code } = targetInfo;
     if (!(code in rooms)) return;
 
