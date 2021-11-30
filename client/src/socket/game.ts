c6d52b6a0a09957cbb979c3fc1d70e615ffd87dd
Ssoco <dlscjs8646@gmail.com>
2021-12-01 05:03:56 +0900

    :sparkles: 클라이언트에서 라이어게임을 위한 socket을 생성

--- client/src/socket/game.ts
+++ client/src/socket/game.ts
@@ -1,59 +1,43 @@
 import { Socket } from 'socket.io-client';
-import {
-  UPDOWN_START,
-  UPDOWN_STOP,
-  LIAR_START,
-  LIAR_STOP,
-} from 'sooltreaming-domain/constant/socketEvent';
+import { UPDOWN_START, LIAR_START, GAME_STOP } from 'sooltreaming-domain/constant/socketEvent';
 
 const game =
   (socket: Socket) =>
-  ({ startUpdown, stopUpdown, startLiar, randomNumRef, keywordRef }) => {
+  ({ startUpdown, startLiar, stopGame, randomNumRef, keywordRef }) => {
     socket.on(UPDOWN_START, (startingSID, randomNum) => {
       randomNumRef.current = randomNum;
       startUpdown(startingSID);
-    });
-    socket.on(UPDOWN_STOP, () => {
-      randomNumRef.current = '';
-      stopUpdown();
     });
 
     socket.on(LIAR_START, (startingSID, keyword) => {
       keywordRef.current = keyword;
       startLiar(startingSID);
     });
-    socket.on(LIAR_STOP, () => {
+    socket.on(GAME_STOP, () => {
       keywordRef.current = '';
-      stopUpdown();
+      stopGame();
     });
 
-    // 업다운 시작과 끝
+    // 게임 시작과 끝
     const requestUpdownStart = () => {
       socket.emit(UPDOWN_START, socket.id);
     };
-    const requestUpdownStop = () => {
-      socket.emit(UPDOWN_STOP);
-    };
-
-    // 라이어 시작과 끝
     const requestLiarStart = () => {
       socket.emit(LIAR_START, socket.id);
     };
-    const requestLiarStop = () => {
-      socket.emit(LIAR_STOP);
+    const requestGameStop = () => {
+      socket.emit(GAME_STOP);
     };
 
     const disconnecting = () => {
       socket.off(UPDOWN_START);
-      socket.off(UPDOWN_STOP);
       socket.off(LIAR_START);
-      socket.off(LIAR_STOP);
+      socket.off(GAME_STOP);
     };
     return {
       requestUpdownStart,
-      requestUpdownStop,
       requestLiarStart,
-      requestLiarStop,
+      requestGameStop,
       disconnecting,
     };
   };
