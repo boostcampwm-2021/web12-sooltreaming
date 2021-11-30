18b41a0b4d9a5ff36f68a3b2bf34f8087102c871
Ssoco <dlscjs8646@gmail.com>
2021-11-30 10:32:59 +0900

    :sparkles: 라이어게임 클라이언트 파일 생성

---
+++ client/src/components/room/games/LiarGame.style.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { COLOR, BTN_STYLE } from '@constant/style';
+
+export const Contents = styled.div`
+  width: 400px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  position: relative;
+`;
+
+export const GameTitle = styled.div`
+  width: 100%;
+  font-weight: bold;
+  color: ${COLOR.titleActive};
+`;
+
+export const GameStopButton = styled.button`
+  ${BTN_STYLE};
+  padding: 5px 10px;
+  font-size: 15px;
+`;
--- client/src/hooks/socket/useGameSocket.ts
+++ client/src/hooks/socket/useGameSocket.ts
@@ -9,10 +9,13 @@ const useGameSocket = () => {
   const dispatch = useDispatch();
   const currentGame = useSelector((state: RootState) => state.room.currentGame);
   const randomNumRef = useRef('');
+
   const onClickUpdown = useCallback(() => {
     socket.requestUpdownStart();
   }, []);
-  const onClickLiar = useCallback(() => {}, []);
+  const onClickLiar = useCallback(() => {
+    socket.requestLiarStart();
+  }, []);
 
   const GameStartHandlerList = useMemo(
     () => ({
@@ -25,14 +28,28 @@ const useGameSocket = () => {
   const startUpdown = (startingSID) => {
     dispatch(setCurrentGame({ title: UP_DOWN, host: startingSID }));
   };
+
   const stopUpdown = () => {
     dispatch(setCurrentGame({ title: '', host: '' }));
   };
 
-  const socket = useMemo(() => Socket.game({ startUpdown, stopUpdown, randomNumRef }), []);
+  const startLiar = (startingSID) => {
+    dispatch(setCurrentGame({ title: LIAR, host: startingSID }));
+  };
+
+  const stopLiar = () => {
+    dispatch(setCurrentGame({ title: '', host: '' }));
+  };
+
+  const socket = useMemo(
+    () => Socket.game({ startUpdown, stopUpdown, startLiar, stopLiar, randomNumRef }),
+    [],
+  );
+
   useEffect(() => {
     if (currentGame.host === Socket.getSID() && !currentGame.title) {
       socket.requestUpdownStop();
+      socket.requestLiarStop();
     }
   }, [currentGame]);
 
