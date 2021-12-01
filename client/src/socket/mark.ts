3bcc945b4aaadd1c8dcecfc87988fad61a79645f
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2021-12-01 16:08:33 +0900

    :recycle: questionMark debounce 추가

--- client/src/socket/mark.ts
+++ client/src/socket/mark.ts
@@ -4,12 +4,20 @@ import { MARK_BROADCAST } from 'sooltreaming-domain/constant/socketEvent';
 const mark = (socket: Socket) => (closure: any) => {
   const { setMarks, removeQuestionMark } = closure;
   let count = 0;
+  let marks = {};
+  let frame;
 
   socket.on(MARK_BROADCAST, ({ x, y }) => {
-    count++;
-    removeQuestionMark(count);
-    setMarks((prev) => {
-      return { ...prev, [count]: { x, y } };
+    marks[++count] = { x, y };
+    if (frame) cancelAnimationFrame(frame);
+    frame = requestAnimationFrame(() => {
+      Object.keys(marks).forEach((cnt) => {
+        removeQuestionMark(cnt);
+      });
+      setMarks((prev) => {
+        return { ...prev, ...marks };
+      });
+      marks = {};
     });
   });
 
