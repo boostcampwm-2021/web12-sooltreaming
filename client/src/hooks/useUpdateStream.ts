d5da4911dd5b9608bc30406c3fb6d596139392b8
yeon52 <52206971+yeon52@users.noreply.github.com>
2021-12-01 18:48:14 +0900

    :bug: webRTC 연결 잘 안되던 버그 해결
    연결 실패하면 재연결 로직 추가
    setRemoteDesciption하기 전에 peerConnections에 peer 추가
    
    Co-Authored-By: PyoSeokHun <55688122+pyo-sh@users.noreply.github.com>
    Co-Authored-By: J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
    Co-Authored-By: Ssoco <dlscjs8646@gmail.com>

--- client/src/hooks/useUpdateStream.ts
+++ client/src/hooks/useUpdateStream.ts
@@ -4,7 +4,7 @@ const useUpdateStream = (elementRef, srcObject, callback = () => {}) => {
   useEffect(() => {
     const element = elementRef.current;
     if (!element) return;
-    element.srcObject = srcObject || null;
+    element.srcObject = srcObject ?? null;
     callback();
   }, [srcObject]);
 };
