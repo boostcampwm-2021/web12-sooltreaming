cb1b7609a62c34fa04e16c3a66220e44388865c8
J200_조현정 <80734219+jyo-jyo@users.noreply.github.com>
2021-12-02 11:34:33 +0900

    :recycle: frontend 예외 처리 수정
    중복 코드 밖으로 다 빼버림

--- client/src/api/rank.ts
+++ client/src/api/rank.ts
@@ -1,8 +1,6 @@
 import request from '@utils/request';
 
 export const getRank = async (type) => {
-  const { status, json } = await request.get({ url: `/rank/${type}` });
-  if (status === 200) {
-    return json;
-  } else throw new Error(json.error.toString());
+  const result = await request.get({ url: `/rank/${type}` });
+  return result;
 };
