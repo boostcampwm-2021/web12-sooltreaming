d24911c67ce727214e4c1902fbe59b23ebce1ce1
Ssoco <dlscjs8646@gmail.com>
2021-12-06 01:45:06 +0900

    :bug: 안쓰는 models 제거

--- server/src/models/LiarGameKeywords.ts
+++
@@ -1,7 +0,0 @@
-import { Schema, model } from 'mongoose';
-import type { LiarGameKeywords } from '@src/types';
-
-const LiarGameKeywordsSchema = new Schema<LiarGameKeywords>({
-  keyword: String,
-});
-export default model<LiarGameKeywords>('LiarGameKeywords', LiarGameKeywordsSchema);
